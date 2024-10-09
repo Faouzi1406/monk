2e24fb47126c639f9324d042deec0b4a181ce46a
Faouzi1406 <faouzib1@outlook.com>
2024-10-09 06:55:05 +0200

    feat: working on the parser

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,50 +1,59 @@
 type ast = Program of statement list * string option
 
 and statement =
-  | Expression of expression
-  | Func of func
-  | Var of var
-  | Type of typef
-  | Implement of implementf
+  | SExpression of expression
+  | SFunc of func
+  | SVar of var
+  | SType of typef
+  | SImplement of implementf
+  | SControllFlow of controll_flow
+  | SBlock of statement list
 
 and expression =
-  | Literal of literal
-  | Matches of matches
-  | FuncValue of func_expr
-  | BinaryOp of expression * op * expression
-  | Condition of expression * comp * expression
+  | ELiteral of literal
+  | ECall of call
+  | EBinaryOp of bin_op
+  | ECondition of cond
 
 and ident = string
 and var = ident * ident option * expression
 and arguments = expression list
-and paramaters = ident * ident option list
-and func = ident * ident option * paramaters * statement list
-and func_expr = ident option * statement list
-and case = Matchcase of ident * arguments option
-and matches = case list * expression
+and paramaters = (ident * ident option) list
+and func = paramaters * statement list
+and call = ident * arguments
+and bin_op = expression * op * expression
+and cond = expression * comp * expression
+
+and controll_flow =
+  | CIf of if_cf
+  | CMatch of (expression * statement) list
+  | CWHile of expression * statement
+
+and if_cf =
+  expression * statement * (expression * statement list) * statement option
 
 and literal =
-  | Int of int
-  | Float of float
-  | Ident of ident
-  | String of string
-  | Char of char
-  | Array of expression list
-  | Object of ident * expression list
+  | LInt of int
+  | LFloat of float
+  | LIdent of ident
+  | LString of string
+  | LChar of char
+  | LArray of expression list
+  | LObject of (ident * expression) list
 
 and typef = ident * expression
-and implementf = ident * statement list
+and implementf = (ident * statement) list
 
 and op =
-  | Plus
-  | Min
-  | Div
-  | Mul
+  | OPlus
+  | OMin
+  | ODiv
+  | OMul
 
 and comp =
-  | More
-  | Less
-  | MoreEq
-  | LessEq
-  | Eq
-  | EqEq
+  | COMore
+  | COLess
+  | COMoreEq
+  | COLessEq
+  | COEq
+  | COEqEq
--- lib/parser.mly
+++ lib/parser.mly
@@ -7,12 +7,31 @@
 %token MATCH
 %token TYPE
 %token IMPLEMENT
-%token IMPLEMENT
+%token LEFTCURLYBRACKET
+%token RIGHTCURLYBRACKET
 %token LEFTBRACKET
 %token RIGHTBRACKET
+%token EQ
+%token EQEQ
+%token MORE
+%token MOREQ
+%token LESS
+%token LESSEQ
+%token ARROWRIGHT
+%token ARROWLEFT
+%token COMMA
+%token COLON
+%token ADD
+%token MIN
+%token MUL
+%token DIV
+%token EOF
 
 
-%start <Ast.ast list> main
+%left PLUS MIN
+%left MUL DIV
+
+%start <Ast.ast> main
 
 %{
     open Ast
@@ -25,8 +44,39 @@ main:
     |  e = list(stmt); EOF;
     { Program (e, None) }
 
+
 stmt:
-        LET 
+        f = func; { f }
+        | e = expr
+        { SExpression e }
+        | v = var;
+        { v }
 
 
 expr:
+    | c = call; { c }
+    | b = b_op; { b }
+
+var: 
+     LET; i = IDENT; t = IDENT?; EQ; e = expr
+    { SVar (i, t, e) }
+
+b_op:
+        lhs = expr; o = op;  rhs = expr; { EBinaryOp(lhs, o, rhs) }
+
+call: 
+        upon = IDENT; a = separated_list(COMMA,expr); { ECall(upon, a)}
+
+func: 
+    params = separated_list(COMMA, param); ARROWRIGHT; LEFTCURLYBRACKET; stmts = list(stmt); RIGHTCURLYBRACKET
+    { SFunc(params, stmts) }
+
+
+op: 
+            | ADD { OPlus }
+            | MIN { OMin }
+            | MUL { OMul }
+            | DIV { ODiv }
+
+param:
+    | lhs = IDENT; COLON; rhs = IDENT?; { lhs, rhs }
